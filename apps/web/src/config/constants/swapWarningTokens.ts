94da388f888e360627d616ba900205b0dc50db0c
yeshealer <yeshealer@gmail.com>
2022-12-04 02:13:43 +0900

    first commit

---
+++ apps/web/src/config/constants/swapWarningTokens.ts
@@ -0,0 +1,25 @@
+import { Token } from '@pancakeswap/sdk'
+import { bscTokens } from '@pancakeswap/tokens'
+import { bscWarningTokens } from 'config/constants/warningTokens'
+
+const { bondly, itam, ccar, bttold, hay, abnbc } = bscTokens
+const { pokemoney, free, safemoon, gala } = bscWarningTokens
+
+interface WarningTokenList {
+  [key: string]: Token
+}
+
+const SwapWarningTokens = <WarningTokenList>{
+  safemoon,
+  bondly,
+  itam,
+  ccar,
+  bttold,
+  pokemoney,
+  free,
+  gala,
+  hay,
+  abnbc,
+}
+
+export default SwapWarningTokens
